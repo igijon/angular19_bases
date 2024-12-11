25f44a8a71afe41b064b88a127939c0e7b3ba317
Inma Gijón <igijoninf@gmail.com>
2024-12-11 12:50:10 +0100

    Bug Fixed. Funcionalidad Counter

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,10 +1,25 @@
-import { Component } from "@angular/core";
+/**Podemos crear el componente con un snippet a-component */
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: `<h1>Hola Counter</h1>`,
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `,
   standalone: false
 })
-export class CounterComponent {
 
+export class CounterComponent {
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
