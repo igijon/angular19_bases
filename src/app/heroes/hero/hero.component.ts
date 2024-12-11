1292bfd2725366214cb891115599488f26390b97
Inma Gijón <igijoninf@gmail.com>
2024-12-11 13:19:15 +0100

    One way data binding

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -9,9 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  /**Two way data binding*/
   public name: string = 'ironman';
   public age: number = 45;
 
+  /** Intentaremos hacer One way data binding, que sólo se cambie en
+   * una única vía
+   */
 
+  //En el futuro lo haremos con un pipe
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
 
+  getHeroDescription():string {
+    return `${this.name} - ${this.age}`;
+  }
 }
