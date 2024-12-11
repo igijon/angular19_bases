c890e7d1ea9a7e1423298d3690f39cb6553a588f
Inma Gijón <igijoninf@gmail.com>
2024-12-11 13:01:14 +0100

    Componentes Hero y List

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,12 @@
+/**ng g c heroes/hero */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  standalone: false,
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+}
