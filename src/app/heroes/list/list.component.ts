7ac196985c24075321fce432bd25b5f0520a8e6e
Inma Gijón <igijoninf@gmail.com>
2024-12-11 17:57:43 +0100

    NgIf al borrar héroes

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
   public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?: string;
+
+  removeLastHero():void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
