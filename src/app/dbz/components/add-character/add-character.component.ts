800d7b7a01eb8e1baa619aef4634b832cde0e63e
Inma Gijón <igijoninf@gmail.com>
2024-12-16 13:26:38 +0100

    Add Character Component

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css'],
+  standalone: false
+})
+export class AddCharacterComponent {
+
+  constructor() { }
+
+
+}
