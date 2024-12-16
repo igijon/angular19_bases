cbc449910c0cb18be369dda6aa34a32c89affeb6
Inma Gijón <igijoninf@gmail.com>
2024-12-16 13:19:57 +0100

    Componente listado

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+  standalone: false
+})
+export class ListComponent {
+
+  constructor() { }
+
+}
