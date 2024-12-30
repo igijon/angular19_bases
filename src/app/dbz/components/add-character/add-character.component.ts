f01635292c65af79c1b2f1397e3808528847ba30
Inma Gijón <igijoninf@gmail.com>
2024-12-30 11:07:52 +0100

    FormsModule y ngModel

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
@@ -8,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCharacterComponent {
 
-  constructor() { }
+  public character: Character = {
+    name: '',
+    power: 0
+  };
 
+  emitCharacter(): void {
+    console.log(this.character);
+  }
 
 }
