11f3f9176857a2c5e7e2adabcdfee8daa335a005
Inma Gijón <igijoninf@gmail.com>
2025-01-09 12:31:45 +0100

    UUID

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,31 +1,36 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 import { Character } from '../interfaces/character.interface';
 
-@Injectable(
-  {
-    providedIn: 'root' //Va a hacer un singleton en toda la aplicación
-  }
-)
+@Injectable({
+  providedIn: 'root', //Va a hacer un singleton en toda la aplicación
+})
 export class DbzService {
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-    },{
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      id: uuid(),
       name: 'Goku',
-      power: 9500
-    }, {
+      power: 9500,
+    },
+    {
+      id: uuid(),
       name: 'Vegeta',
-      power: 7500
-    }];
+      power: 7500,
+    },
+  ];
 
-  onNewCharacter(character: Character): void
-  {
-    this.characters.unshift(character); //Lo inserta al principio
-    //this.characters.push(character);
+  onNewCharacter(character: Character): void {
+    const newCharacter: Character = { id: uuid(), ...character}
+    this.characters.unshift(newCharacter); //Lo inserta al principio
+
   }
 
-  onDeleteCharacter( index: number ) {
+  onDeleteCharacter(index: number) {
     this.characters.splice(index, 1);
   }
-
 }
